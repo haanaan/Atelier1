f8d2ad33a4fdbc987251eacb8b7207b5b997aeeb
faozianikiema <nikiemaf37@gmail.com>
2025-10-21 11:05:50 +0200

    feat(service):les service pour les outils

---
+++ app/src/application_core/application/useCase/OutilsService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\scr\api\application_core\application\useCase;
+
+use charlymatloc\infra\repositories\PDOOutilsRepository;
+use src\api\dto\DetailOutilDto;
+use src\api\dto\OutilCatalogue;
+
+
+
+class OutilsService {
+    private PDOOutilsRepository $outilsRepository;
+    public function __construct( PDOOutilsRepository $outilsRepository) {
+        $this->outilsRepository=$outilsRepository;
+    }
+
+    public function AfficheOutils(): array {
+        $outils = $this->outilsRepository->GetAllOutils();
+        $outilsDTO = [];
+
+        foreach ($outils as $outil) {
+            $outilsDTO[] = new OutilCatalogue(
+                $outil->getNom(),
+                $outil->getImage(),
+                $outil->getNombreExemplaires()
+            );
+        }
+
+        return $outilsDTO;
+    }
+
+    // 2️⃣ Récupérer un seul outil par ID (détail)
+    public function AfficheById(string $id_p): DetailOutilDto {
+        $outil = $this->outilsRepository->GetOutil($id_p);
+
+        if (!$outil) {
+            throw new \Exception("Outil avec l'ID $id_p introuvable");
+        }
+
+        return new DetailOutilDto(
+            $outil->getNom(),
+            $outil->getDescription(),
+            $outil->getImage(),
+            $outil->getCategorie()->getNom() 
+        );
+    }
+}
