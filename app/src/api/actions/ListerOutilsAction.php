13077b3da23614a472e54719ee12303c0e631ccb
Mouad TAJ <mouadtaj.etudes@gmail.com>
2025-10-21 11:06:40 +0200

    Feat(Lister Outils Action)

---
+++ app/src/api/actions/ListerOutilsAction.php
@@ -0,0 +1,34 @@
+<?php
+namespace charlymatloc\api\actions;
+
+use charlymatloc\core\application\ports\spi\repositoryinterfaces\PDOOutilsRepositoryInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ListerOutilsAction
+{
+    private PDOOutilsRepositoryInterface $outilsRepository;
+
+    public function __construct(PDOOutilsRepositoryInterface $outilsRepository)
+    {
+        $this->outilsRepository = $outilsRepository;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $outils = $this->outilsRepository->findAll();
+
+        $data = [];
+        foreach ($outils as $outil) {
+            $data[] = [
+                'id' => $outil->getId(),
+                'nom' => $outil->getNom(),
+                'image' => $outil->getImage(),
+                'exemplaires' => 1
+            ];
+        }
+
+        $response->getBody()->write(json_encode($data, JSON_UNESCAPED_UNICODE));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
