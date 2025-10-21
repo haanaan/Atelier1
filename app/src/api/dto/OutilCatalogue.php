dd4242d64f0be990e2b1c0fba1559499e854ea99
Hanan <hanan.ashrafi@lycee-faure.fr>
2025-10-21 11:36:33 +0200

    Outil Catalogue

---
+++ app/src/api/dto/OutilCatalogue.php
@@ -0,0 +1,17 @@
+<?php
+namespace src\api\dto;
+
+class OutilCatalogue
+{
+    public string $nom;
+    public string $image;
+    public int $nombreExemplaires;
+
+    public function __construct(string $nom, string $image, int $nombreExemplaires)
+    {
+        $this->nom = $nom;
+        $this->image = $image;
+        $this->nombreExemplaires = $nombreExemplaires;
+    }
+
+}
