349f51894168a665c143aee61095e0b15963fd41
Hanan <hanan.ashrafi@lycee-faure.fr>
2025-10-21 11:27:20 +0200

    DTOs

---
+++ app/src/api/dto/DetailOutilDTO.php
@@ -0,0 +1,18 @@
+<?
+namespace src\api\dto;
+
+class DetailOutilDto
+{
+    public string $nom;
+    public string $description;
+    public string $image;
+    public string $categorie;
+
+    public function __construct(string $nom, string $description, string $image, string $categorie)
+    {
+        $this->nom = $nom;
+        $this->description = $description;
+        $this->image = $image;
+        $this->categorie = $categorie;
+    }
+}
