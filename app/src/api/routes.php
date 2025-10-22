45485c675382e2aaa3e277e62f25670f3e1dc78f
Hanan <hanan.ashrafi@lycee-faure.fr>
2025-10-22 14:39:34 +0200

    maj routes

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -9,12 +9,9 @@ return function (\Slim\App $app): \Slim\App {
     $app->get('/api/outils', ListerOutilsAction::class);
     $app->get('/api/outils/{id}', GetOutilsAction::class);
     $app->get('/api/panier/{id}', GetPanierAction::class);
-<<<<<<< HEAD
     $app->post('/api/inscription', InscriptionAction::class);
 
 
-=======
->>>>>>> ab311b01e286380c940dd736a7be80bdd4fac34e
     return $app;
 };
 
