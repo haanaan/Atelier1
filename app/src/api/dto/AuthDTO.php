459674f0aa1a579e307b92a7381390d1a114b1c6
Hanan <hanan.ashrafi@lycee-faure.fr>
2025-10-22 14:33:58 +0200

    inscription api

--- app/src/api/dto/AuthDTO.php
+++ app/src/api/dto/AuthDTO.php
@@ -1,16 +1,43 @@
 <?php
 
-namespace src\api\dto;
+namespace charlymatloc\api\dto;
 
 class AuthDto
 {
+    public string $id;
     public string $email;
-    public string $password;
+    public string $nom;
+    public string $prenom;
+    public ?string $token;
 
-    public function __construct(string $email, string $password)
+    public function __construct(string $id, string $email, string $nom, string $prenom, ?string $token = null)
     {
+        $this->id = $id;
         $this->email = $email;
-        $this->password = $password;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->token = $token;
+    }
 
+    public static function fromUser(object $user, ?string $token = null): self
+    {
+        return new self(
+            $user->getId(),
+            $user->getEmail(),
+            $user->getNom(),
+            $user->getPrenom(),
+            $token
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'email' => $this->email,
+            'nom' => $this->nom,
+            'prenom' => $this->prenom,
+            'token' => $this->token,
+        ];
     }
 }
--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -1,14 +1,16 @@
 <?php
 declare(strict_types=1);
+use charlymatloc\api\actions\InscriptionAction;
 use charlymatloc\api\actions\ListerOutilsAction;
 use charlymatloc\api\actions\GetOutilsAction;
 use charlymatloc\api\actions\GetPanierAction;
 
 return function (\Slim\App $app): \Slim\App {
     $app->get('/api/outils', ListerOutilsAction::class);
-
     $app->get('/api/outils/{id}', GetOutilsAction::class);
     $app->get('/api/panier/{id}', GetPanierAction::class);
+    $app->post('/api/inscription', InscriptionAction::class);
+
 
     return $app;
 };
